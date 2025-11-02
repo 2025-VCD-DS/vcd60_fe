f324bcbe65002d89ca83dea251f2bcb539c20952
pookey1104 <pookey1104@naver.com>
2025-11-03 01:40:49 +0900

    ✏️ Style : Provider에 globalStyles 적용 (#1)
    Provider에 globalStyles 적용

--- src/app/providers/Provider.tsx
+++ src/app/providers/Provider.tsx
@@ -3,10 +3,12 @@
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/styles/theme';
 import EmotionRegistry from '@/lib/emotionRegistry';
+import GlobalStyles from '@/styles/globalStyles';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <EmotionRegistry>
+      <GlobalStyles />
       <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </EmotionRegistry>
   );
