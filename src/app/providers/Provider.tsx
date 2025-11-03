2b04375c9abb8d29a7063f282c8e072ecbd90e7b
pookey1104 <pookey1104@naver.com>
2025-11-04 07:59:25 +0900

    ✨ Feat : 전체 배경 스타일 구현 (#4)
    전체 배경 스타일 구현

--- src/app/providers/Provider.tsx
+++ src/app/providers/Provider.tsx
@@ -4,12 +4,36 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/styles/theme';
 import EmotionRegistry from '@/lib/emotionRegistry';
 import GlobalStyles from '@/styles/globalStyles';
+import Image from 'next/image';
+import styled from '@emotion/styled';
+import bgBackground from '@/assets/background/bg-background.svg?url';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <EmotionRegistry>
       <GlobalStyles />
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Image
+            src={bgBackground}
+            alt="background"
+            fill
+            style={{ objectFit: 'cover', objectPosition: 'center' }}
+            priority
+          />
+          <Overlay>{children}</Overlay>
+        </Container>
+      </ThemeProvider>
     </EmotionRegistry>
   );
 }
+
+const Container = styled.div`
+  position: relative;
+  min-height: 100dvh;
+`;
+
+const Overlay = styled.div`
+  position: relative;
+  z-index: 1;
+`;
